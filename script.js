26698062c1b40290455980a371723d53382e8114
LiliKaori <lilikaori10@gmail.com>
2023-05-15 23:36:35 -0300

    Importar dados de formação dinamicamente em JS

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
+async function fetchData(){
+    const data = await(await fetch("./data/db.json")).json()
 
-async function showProject (){
-    const data= await(await fetch("./data/data.json")).json()
-    const dataProjects = data.projects    
-const newSlides= dataProjects
-    .forEach(data=>document
-    .getElementById(`slide-${data.id}`)
-    .innerHTML=`<img class="slideImage" src="${data.image}" alt="${data.name}"><p class="author-info"><a href="${data.url}" target="_blanck">${data.name}</a></p>`)
- 
-return (newSlides)
+    function showFormation(){        
+        const dataFormations = data.formation    
+        const newBox= dataFormations.map(data=>(document.getElementById("academicFormation")
+        .innerHTML+=`
+        <div class="box">
+            <div class="boxLeft">
+                <div id="imgForm${data.id}" class="imgBox"></div>
+                <h3>${data.initiation_year}-${data.conclusion_year}</h3>
+                <h4>${data.name}</h4>
+            </div>
+            <div class="boxRight">
+                <p>${data.description}</p>
+            </div>
+        </div>
+        `))
+    return newBox
+    }
+    showFormation()
+
+    function showProject (){        
+        const dataProjects = data.projects    
+        const newSlides= dataProjects
+        .forEach(data=>document
+        .getElementById(`slide-${data.id}`)
+        .innerHTML=`
+        <img class="slideImage" src="${data.image}" alt="${data.name}">
+        <p class="author-info">
+            <a href="${data.url}" target="_blanck">${data.name}</a>
+        </p>
+        `)     
+    return (newSlides)    
+    }
+    showProject()
     
-    
-    
-    // console.log(finalAd)
-    // dataProjects.map( data =>(        
-    //           document.querySelector(".slides").innerHTML = `
-    //           <img src="${data.image}" alt="">
-    //                           <p class="author-info">
-    //                                             <a href="${data.url}">Projeto ${data.name}</a>                
-    //                                             </p>              `))
 }
+fetchData()
 
-showProject()
+
+
+
+
+
+
